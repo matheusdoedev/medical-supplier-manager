e2626604d27bd89818d70c13711521d1b9f61689
@matheusdoedev <matheusdoe.dev@gmail.com>
2023-09-24 15:35:15 -0300

    feat: new dashboard and create medicine routes added

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { Home } from '@/views'
+import { CreateMedicine, Home, Login } from '@/views'
 
 export const router = createBrowserRouter([
   {
     path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/dashboard',
     element: <Home />,
+  },
+  {
+    path: '/create-medicine',
+    element: <CreateMedicine />,
   },
 ])
