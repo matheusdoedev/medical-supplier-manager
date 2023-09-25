89cb9d5aff11fe70401a2bd16b98c782673c9e5c
@matheusdoedev <matheusdoe.dev@gmail.com>
2023-09-25 20:53:58 -0300

    feat: header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import { styled } from 'styled-components'
+
+import { Container, Logo } from '@/components'
+
+const Header = () => {
+  return (
+    <HeaderWrapper data-testid="header">
+      <Container>
+        <Logo variant="white" />
+      </Container>
+    </HeaderWrapper>
+  )
+}
+
+const HeaderWrapper = styled.header`
+  padding: 16px;
+  background: ${({ theme }) => theme.colors.secondary['500']};
+`
+
+export default Header
