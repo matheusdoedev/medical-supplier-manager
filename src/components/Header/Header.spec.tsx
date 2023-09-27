926aed14f865d74f6ab496311755d738773faefd
@matheusdoedev <matheusdoe.dev@gmail.com>
2023-09-27 14:50:23 -0300

    feat: it adds logout feature to header component

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,9 +1,16 @@
+import { vi } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 
 import Header from '.'
 
 import { theme } from '@/styles'
+
+vi.mock('react-router-dom', () => {
+  return {
+    useNavigate: vi.fn(),
+  }
+})
 
 describe('Header component', () => {
   it('should render', () => {
